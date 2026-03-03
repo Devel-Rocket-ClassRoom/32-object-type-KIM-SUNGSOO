ae21cedfdf29ebc2eafd7560094451a5ef8486d2
KIM-SUNGSOO <133596829+KIM-SUNGSOO@users.noreply.github.com>
2026-03-03 17:50:20 +0900

    Prac

--- CodingPractice/Program.cs
+++ CodingPractice/Program.cs
@@ -1,4 +1,136 @@
 using System;
 
-// README.md를 읽고 코드를 작성하세요.
-Console.WriteLine("코드를 작성하세요.");
+
+class CoPrac
+{
+    static void Main()
+    {
+        //Prac_1();
+        //Prac_2();
+        //Prac_3_1();
+        //Prac_3_2();
+        //Prac_4_1();
+        //Prac_4_2();
+        //Prac_5();
+        //Prac_6();
+        //Prac_7_1();
+        Prac_7_2();
+    }
+
+    static void Prac_1()
+    {
+        object obj1 = 42;
+        object obj2 = 3.14;
+        object obj3 = "Hello";
+        object obj4 = true;
+
+        Console.WriteLine(obj1);
+        Console.WriteLine(obj2);
+        Console.WriteLine(obj3);
+        Console.WriteLine(obj4);
+    }
+
+    static void Prac_2()
+    {
+        Stack stack = new Stack();
+        stack.Push("sausage");
+        string s = (string)stack.Pop();
+        Console.WriteLine(s);
+
+        stack.Push(3);
+        int three = (int)stack.Pop();
+        Console.WriteLine(three);
+
+    }
+
+    static void Prac_3_1()
+    {
+        int i = 1234;
+        object o = i;
+
+        Console.WriteLine(i);
+    }
+
+    static void Prac_3_2()
+    {
+        object obj = 1234;
+        int i = (int)obj;
+        Console.WriteLine(i);
+    }
+
+    static void Prac_4_1()
+    {
+        int i = 42;
+        object obj = i;
+        Console.WriteLine(obj);
+
+    }
+
+    static void Prac_4_2()
+    {
+        Point p = new Point() { X = 10, Y = 20 };
+        IDisplayable d = p;
+        Console.WriteLine(d.GetType().Name);
+    }
+
+    static void Prac_5()
+    {
+        int i = 3;
+        object obj = i;
+        i = 5;
+
+        Console.WriteLine($"원본: {i}");
+        Console.WriteLine($"박싱된 값: {obj}");
+
+    }
+
+    static void Prac_6()
+    {
+        object obj = 42;
+        int i = (int)obj;
+        Console.WriteLine($"언박싱 성공 : {i}");
+
+        try
+        {
+            long l = (long)obj;
+        }
+        catch(InvalidCastException)
+        {
+            Console.WriteLine("잘못된 타입으로 언박싱 시도");
+        }
+
+        int temp = (int)obj;
+        long correcct = temp;
+        Console.WriteLine($"올바른 변환 : {correcct}");
+    }
+
+    static void Prac_7_1()
+    {
+        int sum = 0;
+        for (int i = 0; i < 1000; i++)
+        {
+            object boxed = i;
+            sum += (int)boxed;
+        }
+        Console.WriteLine($"박싱 사용: {sum}");
+
+        int sum1 = 0;
+        for (int i = 0; i < 1000;i++)
+        {
+            sum1 += i;
+        }
+        Console.WriteLine($"직접 처리: {sum1}");
+    }
+
+    static void Prac_7_2()
+    {
+        GenericStack<int> genricStack = new GenericStack<int>();
+        genricStack.Push(42);
+        int value = genricStack.Pop();
+        Console.WriteLine(value);
+    }
+}
+
+
+
+
